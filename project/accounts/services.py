9c122fe0ca8a6ebddca7afa903d78f7103e9a1b9
weird-ego <w31rd.eg0@gmail.com>
2021-02-05 16:24:35 +0200

    Task 4

--- project/accounts/services.py
+++ project/accounts/services.py
@@ -22,5 +22,19 @@ def perform_deposit(user, amount: Decimal):  # for external typechecks
         return transaction
 
 
-def perform_withdrawal(user, amount):
-    pass
+def perform_withdrawal(user, amount: Decimal):  # for external typechecks
+    with atomic():
+        account = Account.object.get(user=user)
+        if account.balance < amount:
+            raise ValueError('Not enough funds')  # debatable
+                                                  # could also return bool
+                                                  # marking success/fail
+        account.balance -= amount
+        account.save()
+
+        transaction = Transaction.objects.create(
+            account=account,
+            transaction_type=Transaction.TRANSACTION_TYPE_WITHDRAWAL
+        )
+
+        return transaction
