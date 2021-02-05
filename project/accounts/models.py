a637c1f8358faaf0a0fae27e2a0ee096411edeff
weird-ego <w31rd.eg0@gmail.com>
2021-02-05 16:22:01 +0200

    Task 2

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from .config import decimal_places
+
 
 class Account(models.Model):
     """
@@ -8,7 +10,7 @@ class Account(models.Model):
     """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts')
-    balance = models.FloatField('balance', default=0)
+    balance = models.DecimalField(decimal_places=decimal_places, max_digits=20)
 
 
 class Transaction(models.Model):
